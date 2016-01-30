92a2721ef872297e456b70891df74d509a64d489
Rennar <jwilliamson@knights.ucf.edu>
2016-01-30 11:45:06 -0500

    Basic Firing
    Player shoots basic bullets. Bullets have colliders, but no collisions
    set up yet.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,62 +3,85 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-    public GameObject model;
-    public float TimeScale = 1f;
+	public GameObject model;
+	public float TimeScale = 1f;
 
-    // Make these into constants of another class later...
-    float stageWidth = 1500f;
-    float stageHeight = 950f;
-    float moveSpeed = 10f;
-    float precisionSpeed = 5f;
-    float shipTilt = 0f;
+	// Make these into constants of another class later...
+	float stageWidth = 1500f;
+	float stageHeight = 950f;
+	float moveSpeed = 10f;
+	float precisionSpeed = 5f;
+	float shipTilt = 0f;
 
-    // Use this for initialization
-    void Start()
-    {
+	public Transform gunR;
+	public Transform gunL;
+	float shootCool;
+	float shootTimer;
+	public GameObject bullet;
 
-    }
+	// Use this for initialization
+	void Start()
+	{
+		shootCool = .2f;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        Time.timeScale = TimeScale;
+	// Update is called once per frame
+	void Update()
+	{
+		Time.timeScale = TimeScale;
 
-        // Get input from controller.
-        float hSpeed = Mathf.Round(Input.GetAxisRaw("Horizontal"));
-        float vSpeed = Mathf.Round(Input.GetAxisRaw("Vertical"));
+		// Get input from controller.
+		float hSpeed = Mathf.Round(Input.GetAxisRaw("Horizontal"));
+		float vSpeed = Mathf.Round(Input.GetAxisRaw("Vertical"));
 
-        // Slightly slower when moving diagonally.
-        if (hSpeed != 0 && vSpeed != 0){
-            hSpeed *= 0.7f;
-            vSpeed *= 0.7f;
-        }
+		// Slightly slower when moving diagonally.
+		if (hSpeed != 0 && vSpeed != 0){
+			hSpeed *= 0.7f;
+			vSpeed *= 0.7f;
+		}
 
-        // Adjust for precision mode.
-        if (Input.GetButton("Precision"))
-        {
-            hSpeed *= precisionSpeed;
-            vSpeed *= precisionSpeed;
-        }
-        else
-        {
-            hSpeed *= moveSpeed;
-            vSpeed *= moveSpeed;
-        }
+		if (Input.GetButtonDown ("Primary")) {
+			StartCoroutine ("Firing");
+		}
 
-        shipTilt = shipTilt * 0.8f - (2 * hSpeed) * 0.2f;
+		// Adjust for precision mode.
+		if (Input.GetButton("Precision"))
+		{
+			hSpeed *= precisionSpeed;
+			vSpeed *= precisionSpeed;
+		}
+		else
+		{
+			hSpeed *= moveSpeed;
+			vSpeed *= moveSpeed;
+		}
 
-        transform.position = new Vector3(
-            Mathf.Clamp(transform.position.x + hSpeed, -stageWidth / 2, stageWidth / 2),
-            Mathf.Clamp(transform.position.y + vSpeed, -stageHeight / 2, stageHeight / 2),
-            0
-        );
+		shipTilt = shipTilt * 0.8f - (2 * hSpeed) * 0.2f;
 
-        model.transform.rotation = Quaternion.Euler(0f, shipTilt, 0f);
-    }
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x + hSpeed, -stageWidth / 2, stageWidth / 2),
+			Mathf.Clamp(transform.position.y + vSpeed, -stageHeight / 2, stageHeight / 2),
+			0
+		);
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        GetComponent<AudioSource>().Play();
-    }
+		model.transform.rotation = Quaternion.Euler(0f, shipTilt, 0f);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		GetComponent<AudioSource>().Play();
+	}
+
+	void Shoot(){
+		Instantiate (bullet, gunL.position, Quaternion.identity);
+		Instantiate (bullet, gunR.position, Quaternion.identity);
+	}
+
+	IEnumerator Firing(){
+		while(Input.GetButton("Primary")){
+			Shoot();
+			yield return new WaitForSeconds(shootCool);
+		}
+		yield break;
+	}
 }
---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBullet : MonoBehaviour {
+
+	float speed;
+
+	float lifeTimer;
+
+	// Use this for initialization
+	void Start () {
+		speed = 500;
+		lifeTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (0, Time.deltaTime * speed, 0);
+		lifeTimer += Time.deltaTime;
+		if (lifeTimer > 8) {
+			Destroy (gameObject);
+		}
+	}
+
+	void FixedUpdate(){
+
+	}
+
+	void OnTriggerEnter2D (){
+
+	}
+}
