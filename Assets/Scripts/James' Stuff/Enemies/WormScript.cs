2c587eb76da6dfd87d93e3b05e0db398a5ddf679
Rennar <jwilliamson@knights.ucf.edu>
2016-02-25 18:35:43 -0500

    messing around with worm

--- Assets/Scripts/James' Stuff/Enemies/WormScript.cs
+++ Assets/Scripts/James' Stuff/Enemies/WormScript.cs
@@ -24,16 +24,22 @@ public class WormScript : MonoBehaviour {
 		speed = 600;
 		distance = 70;
 		smooth = .02f;
+		//StartCoroutine ("FollowDelay");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		target = head.position + head.up*distance;
+		
+		Follow ();
+	}
+
+	void Follow(){
+
 		foreach (Transform segment in segments) {
 			 dir = target - segment.position;
-			/*angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
-			segment.rotation = Quaternion.AngleAxis (angle + 90, Vector3.forward);
-			*/
+			//angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
+			//segment.rotation = Quaternion.AngleAxis (angle + 90, Vector3.forward);
+
 
 			//segment.LookAt (target, Vector3.forward);
 
@@ -44,6 +50,41 @@ public class WormScript : MonoBehaviour {
 
 			segment.position = Vector3.MoveTowards (segment.position,target,speed*Time.deltaTime);
 			target = segment.position + segment.up *distance;
+		}
+		for(int i = segments.Count - 1; i>0; i--){
+			target = segments[i-1].position + segments[i-1].up *distance;
+			dir = target - segments[i].position;
+			segments[i].rotation = Quaternion.LookRotation(Vector3.forward,dir);
+			segments[i].Rotate (0, 0, 180);
+			segments[i].position = Vector3.MoveTowards (segments[i].position,target,speed*Time.deltaTime);
+		}
+		target = head.position + head.up*distance;
+		dir = target - segments[0].position;
+		segments[0].rotation = Quaternion.LookRotation(Vector3.forward,dir);
+		segments[0].Rotate (0, 0, 180);
+		segments[0].position = Vector3.MoveTowards (segments[0].position,target,speed*Time.deltaTime);
+	}
+
+	IEnumerator FollowDelay(){
+		while (true) {
+			target = head.position + head.up*distance;
+			foreach (Transform segment in segments) {
+				dir = target - segment.position;
+				//angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
+				//segment.rotation = Quaternion.AngleAxis (angle + 90, Vector3.forward);
+
+
+				//segment.LookAt (target, Vector3.forward);
+
+				//Quaternion lookRot
+				segment.rotation = Quaternion.LookRotation(Vector3.forward,dir); 
+				//segment.rotation = Quaternion.Lerp(segment.rotation,lookRot,Time.deltaTime*smooth);
+				segment.Rotate (0, 0, 180);
+
+				segment.position = Vector3.MoveTowards (segment.position,target,speed*Time.deltaTime);
+				target = segment.position + segment.up *distance;
+				yield return new WaitForSeconds (.00001f);
+			}
 		}
 	}
 }
