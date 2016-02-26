48e8113c4fbfc7c7d11b8e7f3ab8eda530bac34e
zachary-goodless <zgoodless@knights.ucf.edu>
2016-02-26 15:48:42 -0500

    Bullet graphic options (painstakingly) implemented for small bullet types.

--- Assets/Scripts/James' Stuff/ShootTestNormal.cs
+++ Assets/Scripts/James' Stuff/ShootTestNormal.cs
@@ -5,13 +5,9 @@ using UnityEngine.UI;
 public class ShootTestNormal : MonoBehaviour {
     float angle = 0;
     int timer = 0;
-
-    [Range(1, 120)]
+    
     public int count = 5;
-    [Range(1, 120)]
     public int rate = 60;
-    public bool randomized = false;
-    public bool curving = false;
     public AudioClip sfx;
     public GameObject text;
 
@@ -31,48 +27,19 @@ public class ShootTestNormal : MonoBehaviour {
 		} else {
 			onScreen = false;
 		}
-
-
-        if (Input.GetKeyDown("1"))
-        {
-            if (count < 80) {
-                count++;
-            }
-        }
-
-        if (Input.GetKeyDown("2"))
-        {
-            if (rate > 1)
-            {
-                rate--;
-            }
-        }
+        
 		if (onScreen) {
 			if (timer % rate == 0) {
+                angle = Random.Range (0, 360);
 
-				if (randomized) {
-					angle = Random.Range (0, 360);
-				} else {
-					angle += 444 / count;
-				}
 
 				for (int i = 0; i < count; i++) {
-					if (curving) {
-						float curveAmount = Random.Range (-0.5f, 0.5f);
-						BulletManager.ShootBullet (transform.position, 20, angle + i * 360 / count, -0.5f, 2, curveAmount);
-						BulletManager.AddAction (new BulletAction (60, true, 0, 0, 0.7f, 18, -curveAmount));
-					} else {
-						BulletManager.ShootBullet (transform.position, 20, angle + i * 360 / count, -0.5f, 2, 0);
-						BulletManager.AddAction (new BulletAction (60, true, 0, 0, 0.7f, 18, 0));
-					}
+					BulletManager.ShootBullet (transform.position, 20, angle + i * 360 / count, -0.5f, 2, 0,BulletType.PinkOrb);
+					BulletManager.AddAction (new BulletAction (60, true, 0, 0, 0.7f, 18, 0));
 				}
 				GetComponent<AudioSource> ().PlayOneShot (sfx);
 			}
 		}
-
-        if (timer % 5 == 0) {
-            //text.GetComponent<Text>().text = "Number of bullets onscreen: " + GameObject.FindGameObjectsWithTag("Bullet").Length + "\nFiring " + count + " bullets every " + rate + " frames.";
-        }
 
         timer++;
     }
