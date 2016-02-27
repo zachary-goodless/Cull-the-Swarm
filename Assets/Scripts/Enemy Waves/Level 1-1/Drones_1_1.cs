f0c1e3301afc27bb04e633771020c6b2888a043f
Rennar <jwilliamson@knights.ucf.edu>
2016-02-27 13:57:09 -0500

    Drone Wave script

--- Assets/Scripts/Enemy Waves/Level 1-1/Drones_1_1.cs
+++ Assets/Scripts/Enemy Waves/Level 1-1/Drones_1_1.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Drones_1_1 : MonoBehaviour {
-	//We'll need to assign this with .AddComponent in the spawns. 
+	//We'll need to assign this with .AddComponent in the spawns.
 
 	public string pattern;
 	bool ongoing;
@@ -20,5 +20,19 @@ public class Drones_1_1 : MonoBehaviour {
 	}
 	
 	//Sput your coroutines here.
+	IEnumerator Test(){
+		//not sure if you are going to keep the same pattern for the duration of each wave/give multiple patterns to things; if so we'll need to change up how we do this a bit. 
+		/*bool condition;
+		 * float seconds;
+		 * ongoing = true;
+		 * While(condition){
+		 * Do Stuff!
+		 * yield return new WaitForSeconds(seconds);
+		 * 
+		 * }
+		 * ongoing = false;
+		 */
+		yield break;
+	}
 	
 }
