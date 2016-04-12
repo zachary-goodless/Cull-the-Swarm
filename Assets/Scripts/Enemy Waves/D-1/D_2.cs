51b7875f137eef8ba8f85aa4276dee97e5ad4992
Michael <reckofm@knights.ucf.edu>
2016-04-12 14:08:15 -0400

    Re-Adjustment for strings in code.

--- Assets/Scripts/Enemy Waves/D-1/D_2.cs
+++ Assets/Scripts/Enemy Waves/D-1/D_2.cs
@@ -28,7 +28,7 @@ public class D_2 : MonoBehaviour
         yield return new WaitForSeconds(10f);
         waves.StartBigW(drone);
         yield return new WaitForSeconds(10f);
-        waves.StartSpawnLinear(drone, 5, 1, 300, 1, -1, 50, waves.leftScreen, waves.upScreen, Quaternion.identity, 200, 0);
+        waves.StartSpawnLinear(drone, 5, 1, 300, 1, -1, 50, waves.leftScreen, waves.upScreen, Quaternion.identity, 200, 0, "");
         yield return new WaitForSeconds(9f);
         waves.StartLR6Alternate(turret);
         yield return new WaitForSeconds(6.5f);
@@ -39,13 +39,13 @@ public class D_2 : MonoBehaviour
         yield return new WaitForSeconds(10f);
         waves.StartFourFromBelow(drone);
         yield return new WaitForSeconds(9f);
-        waves.StartSpawnDive(turret, 5, 3, 300, 10, 100, 1, -1, 50, waves.leftScreen, waves.upScreen, Quaternion.identity, 0, 200);
+        waves.StartSpawnDive(turret, 5, 3, 300, 10, 100, 1, -1, 50, waves.leftScreen, waves.upScreen, Quaternion.identity, 0, 200, "");
         yield return new WaitForSeconds(10f);
-        waves.StartSpawnLinear(turret, 2, 4, 350, -1, 1, 50, waves.rightScreen, waves.downScreen, Quaternion.identity, 0, 0);
-        waves.StartSpawnLinear(turret, 2, 4, 350, 1, 1, 50, waves.leftScreen, waves.downScreen, Quaternion.identity, 0, 0);
+        waves.StartSpawnLinear(turret, 2, 4, 350, -1, 1, 50, waves.rightScreen, waves.downScreen, Quaternion.identity, 0, 0, "");
+        waves.StartSpawnLinear(turret, 2, 4, 350, 1, 1, 50, waves.leftScreen, waves.downScreen, Quaternion.identity, 0, 0, "");
         yield return new WaitForSeconds(10f);
-        waves.StartSpawnLinear(drone, 4, 4, 350, -1, -1, 50, waves.rightScreen, waves.upScreen, Quaternion.identity, 0, 0);
-        waves.StartSpawnLinear(drone, 4, 4, 350, 1, -1, 50, waves.leftScreen, waves.upScreen, Quaternion.identity, 0, 0);
+        waves.StartSpawnLinear(drone, 4, 4, 350, -1, -1, 50, waves.rightScreen, waves.upScreen, Quaternion.identity, 0, 0, "");
+        waves.StartSpawnLinear(drone, 4, 4, 350, 1, -1, 50, waves.leftScreen, waves.upScreen, Quaternion.identity, 0, 0, "");
         yield return new WaitForSeconds(30f);
         sf.Fade();
         finished.handleLevelCompleted((SceneIndex)SceneManager.GetActiveScene().buildIndex);
