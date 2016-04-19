2600857b2ee87baae2d71d08f7f54ae3a39603ca
zachary-goodless <zgoodless@knights.ucf.edu>
2016-04-19 19:17:20 -0400

    Duplicate stuff

--- Assets/HoloDuplicate.cs
+++ Assets/HoloDuplicate.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class HoloDuplicate : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-        transform.position = GameObject.FindObjectWithTag("Player").transform.position;
-        Invoke("Destroy(gameObject)",5);
-	}
+	void Start ()
+    {
+        GameObject g = GameObject.FindWithTag("Duplicate");
+        transform.position = g.transform.position;
+        Invoke("Destroy(gameObject)", 5);
+    }
 	
 	// Update is called once per frame
 	void Update () {
