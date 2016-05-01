ca4277ca42911b30282d5df175c7d8c48f81ab1e
zachary-goodless <zgoodless@knights.ucf.edu>
2016-05-01 01:37:21 -0400

    Shadow ship and termite queen sequence setup (with temporary patterns and low health for testing), moth scene improved lighting, spider implemented with temporary pattern

--- Assets/Scripts/Boss/ShipPatterns.cs
+++ Assets/Scripts/Boss/ShipPatterns.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 
 public class ShipPatterns : MonoBehaviour {
 
+    public GameObject trueLastBoss;
+    public GameObject firstSong;
+    public GameObject secondSong;
+
     bool phaseJustChanged = false;
     int currentPhase = 0;
     bool bossDead = false;
@@ -27,6 +31,41 @@ public class ShipPatterns : MonoBehaviour {
         }
 	}
 
+    public void TriggerNextBoss()
+    {
+        StartCoroutine(TriggerNextBossCo());
+    }
+
+    public IEnumerator TriggerNextBossCo()
+    {
+        Debug.Log("Starting transition coroutine.");
+        StartCoroutine(SwitchSongs());
+        StartCoroutine(FlyAway());
+        // to-do: explosion effect when going offscreen
+
+        trueLastBoss.GetComponent<CircleCollider2D>().enabled = true;
+        trueLastBoss.GetComponent<Boss>().enabled = true;
+        trueLastBoss.GetComponent<QueenPatterns>().enabled = true;
+        yield return new WaitForSeconds(10f);
+        gameObject.SetActive(false);
+        yield break;
+    }
+
+    IEnumerator SwitchSongs()
+    {
+        AudioSource song1 = firstSong.GetComponent<AudioSource>();
+        AudioSource song2 = secondSong.GetComponent<AudioSource>();
+        for(float v = 100; v >= 0; v--)
+        {
+            song1.volume = v/100f;
+            yield return new WaitForSeconds(0.03f);
+        }
+        song1.Stop();
+        song2.Play();
+        Debug.Log("Done switching songs.");
+        yield break;
+    }
+
     IEnumerator FlyIn()
     {
         for(int i = 0; i < 120; i++)
@@ -37,6 +76,15 @@ public class ShipPatterns : MonoBehaviour {
         ChooseRandomPattern();
     }
 
+
+    IEnumerator FlyAway()
+    {
+        for (int i = 0; i < 360; i++)
+        {
+            transform.Translate(new Vector3(0, 1500f / 360f, 0));
+            yield return null;
+        }
+    }
     void ChooseRandomPattern()
     {
         if(b.health < 0) { return; }
@@ -84,7 +132,7 @@ public class ShipPatterns : MonoBehaviour {
     IEnumerator Pattern1()
     {
 
-        for(int i = 0; i < 50; i++)
+        for(int i = 0; i < 40; i++)
         {
             if (phaseJustChanged)
             {
@@ -93,15 +141,24 @@ public class ShipPatterns : MonoBehaviour {
                 yield break;
             }
 
-            if(i%15 == 0){
+            if (i % 4 == 0 && i > 4) {
+                float x = transform.position.x + Random.Range(-120f, 120f);
+                float y = transform.position.y + Random.Range(-30f, 30f);
+                Vector2 pos = new Vector2(x, y);
+                for (int j = 0; j < 4; j++){
+                    BulletManager.ShootBullet(pos, 6f + j / 2f, BulletManager.AngleToPlayerFrom(pos), BulletType.PurpleDarkArrow);
+                }
+            }
+
+            if (i%15 == 0){
                 Vector2 pt = GameObject.FindGameObjectWithTag("Player").transform.position;
                 b.MoveTo(new Vector2(pt.x, Mathf.Clamp(pt.y + 100f,300f,0f)));
             }
             
-            BulletManager.ShootBullet(new Vector2(transform.position.x-64f,transform.position.y),16f,270f,BulletType.RedFire);
-            BulletManager.ShootBullet(new Vector2(transform.position.x+64f,transform.position.y),16f,270f,BulletType.RedFire);
+            BulletManager.ShootBullet(new Vector2(transform.position.x-96f,transform.position.y),10f + (i % 4),270f,BulletType.RedFire);
+            BulletManager.ShootBullet(new Vector2(transform.position.x+96f,transform.position.y),10f + (i % 4), 270f,BulletType.RedFire);
 
-            yield return new WaitForSeconds(0.15f);
+            yield return new WaitForSeconds(0.2f);
         }
         yield return new WaitForSeconds(2f);
         ChooseRandomPattern();
