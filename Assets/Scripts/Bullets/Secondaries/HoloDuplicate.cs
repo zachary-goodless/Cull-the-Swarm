d5fb36a9963cc505e4a150537902a7b456390210
zachary-goodless <zgoodless@knights.ucf.edu>
2016-05-01 12:58:38 -0400

    Duplicate script

---
+++ Assets/Scripts/Bullets/Secondaries/HoloDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoloDuplicate : MonoBehaviour {
+    GameObject holo;
+
+	// Use this for initialization
+	void Start ()
+    {
+        holo = Resources.Load("PlayerBullets/Hologram") as GameObject;
+    }
+
+   public void SpawnHolo()
+    {
+        GameObject p = GameObject.FindWithTag("Player");
+        GameObject.Instantiate(holo, new Vector3(p.transform.position.x, p.transform.position.y, 0f), Quaternion.Euler(p.transform.eulerAngles));
+        holo.transform.position = p.transform.position;
+        Invoke("Destroy(gameObject)", 5);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
